d8994565ddecbdd59ec65e02633802a60f71f5ee
Ted Papaioannou <tap792@gmail.com>
2013-12-17 15:58:44 -0500

    some new stuff

--- cpu/cpu.v
+++ cpu/cpu.v
@@ -70,7 +70,12 @@ module cpu
     wire [15:0] branch_offset_wire;
     wire [31:0] reg_address_wire;
     wire [31:0] pc_to_address_wire;
-    wire [31:0] instr_mem_instruction;
+    wire [31:0] instr_mem_instruction_wire;
+    wire [4:0] waddr_wire; // mux output
+    wire [31:0] wdata_wire; // mux output
+    wire [31:0] operand0_wire; // mux output
+    wire [31:0] sign_extension_out_wire;
+
 
 
 	//---------------------------------------------------------------
@@ -128,31 +133,31 @@ module cpu
     control_unt cpu_control_unit
     (
       .rst  (rst),
-      .instruction  (),
-      .data_mem_wren  (),
+      .instruction  (instr_mem_instruction),
+      .data_mem_wren  (alu_zero_wire),
       .reg_file_wren  (),
       .reg_file_dmux_select(),
-      .reg_file_rmux_select (),
-      .alu_mux_select (),
-      .alu_control (),
-      .alu_zero (),
-      .alu_shamt (),
-      .pc_control ()
+      .reg_file_rmux_select (reg_file_rmux_select),
+      .alu_mux_select (alu_mux_select_wire),
+      .alu_control (alu_control_wire),
+      .alu_zero (alu_zero_wire),
+      .alu_shamt (alu_shamt_wire),
+      .pc_control (pc_control_wire)
     );
 
     mux_2_to1 cpu_mux_2to1_reg_file
     (
-      .in0 (),
-      .in1 (),
-      .out (),
+      .in0 (instr_mem_instruction[20:16]),
+      .in1 (instr_mem_instruction[15:11]),
+      .out (waddr_wire),
       .sel ()
     );
 
     mux_2_to1 cpu_mux_2to1_alu
     (
       .in0 (),
-      .in1 (),
-      .out (),
+      .in1 (sign_extension_out_wire),
+      .out (operand1_wire),
       .sel ()
     );
 
@@ -166,8 +171,8 @@ module cpu
 
     sign_extension cpu_sign_extension
     (
-      .in (),
-      .out ()
+      .in (instr_mem_instruction[15:0]),
+      .out (sign_extension_out_wire)
     );
 
 	//---------------------------------------------------------------
